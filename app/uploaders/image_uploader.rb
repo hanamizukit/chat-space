3070e211d5b696bae181cea8332e123e4518abbb
hanamizukit <56493614+hanamizukit@users.noreply.github.com>
2019-10-25 12:22:37 +0900

    非同期通信、変更
    画像の大きさを変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
-  process resize_to_fit: [500, 500]
+  process resize_to_fit: [250, 250]
   
   # Choose what kind of storage to use for this uploader:
   storage :file
